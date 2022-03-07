7f0fb7ebaaa25806e65e4cc2e9b10b56af6a9dba
ArevikGevorgyan <100373821+ArevikGevorgyan@users.noreply.github.com>
2022-03-07 20:16:29 +0400

    Add files via upload

---
+++ Homework1.js
@@ -0,0 +1,103 @@
+5.teriiiiii
+
+let a=+prompt()
+let b=+prompt()
+if (a%b=0 || b%a=0) {
+    console.log("1")
+}  else {
+    console.log("0")
+}
+
+Kam
+
+let a=+prompt()
+let b=+prompt()
+a%b=0 || b%a=0 ? console.log("1") : console.log("0")
+
+bayc errora talis
+
+
+
+
+6.-----
+
+let a=+prompt()
+switch (a) {
+    case a=2: {
+        console.log("February")
+        break;
+    }
+        case a=4: {
+        console.log("April")
+        break;
+    }
+        case a=11: {
+        console.log("November")
+        break;
+    }
+}
+
+
+
+4.
+
+let a=+prompt()
+a<0 ? console.log("yes") : console.log("no")
+
+
+
+1.
+var a=14
+var b=5
+a-b
+9
+var a=14
+var b=5
+a+b
+19
+a-b
+9
+a*b
+70
+a/b
+2.8
+a%b
+4
+var a=99
+var b=11
+a+b
+110
+a-b
+88
+a*b
+1089
+a/b
+9
+a%b
+0
+var a=81
+var b=16
+a+b
+97
+a-b
+65
+a*b
+1296
+a/b
+5.0625
+a%b
+1
+
+7.
+let a=+prompt()
+let b=+prompt()
+let c=+prompt()
+
+if(a>b && a>c) {
+    console.log("a is maximum")
+    }  else if (b>a && b>c) {
+        console.log("b is maximum")
+    }  else if (c>a && c>b) {  
+        console.log("c is maximum")
+    }
+
